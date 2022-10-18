012c7421f1d50659829c8930fd39b56f212007b5
Anastasiia <anasta_ok@mail.ru>
2022-10-18 19:14:40 +0300

    creating react of pages

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import GlobalStyle from "./globalStyles";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/HomePage/Home";
+import Services from "./pages/Services/Services";
+import Products from "./pages/Products/Products";
+import SignUp from "./pages/SignUp/SignUp";
 import Footer from "./components/Footer/Footer";
-import Pricing from "./components/Pricing";
 
 function App() {
   return (
@@ -13,9 +15,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
       <Footer />
-      <Pricing />
     </Router>
   );
 }
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import InfoSection from "../../components/InfoSection";
 import Pricing from "../../components/Pricing";
-import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./Date";
+import { homeObjOne, homeObjThree } from "./Data";
 
-const Home = () => {
+function Services() {
   return (
     <>
+      <Pricing />
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjThree} />
-      <Pricing />
-      <InfoSection {...homeObjTwo} />
-      <InfoSection {...homeObjFour} />
     </>
   );
-};
+}
 
-export default Home;
+export default Services;
